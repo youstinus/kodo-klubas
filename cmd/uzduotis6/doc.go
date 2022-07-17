3b9a3c8d7b0b222824eae88a6b87c56150ca25d8
youstinus <youstinus@gmail.com>
2022-07-17 13:03:09 +0300

    uzduotis 6 kodoklubas

--- cmd/uzduotis6/doc.go
+++ cmd/uzduotis6/doc.go
@@ -1,4 +1,26 @@
-/* 6.
- */
+/* 6. Kreksint nuo vieno tvenkinio iki kito
+Ši patirtis ančiukams buvo išties nervinga, tačiau jie jau išėjo iš interviu ir iš pastato. Laikas informuoti šeimą apie tai, kaip sekėsi. Ne, antys nenaudoja SMS. Jie turi savo sistemą – QQS. Jos šifras grindžiamas supaprastinta labai populiarios kodavimo mašinos versija.
+
+Šiame kode naudojami 3 lietuviškos abėcėlės masyvai [A,Ą,B,C,Č,D,E,Ę,Ė,F,G,H,I,Į,Y,J,K,L,M,N,O,P,R,S,Š,T,U,Ų,Ū,V,Z,Ž], kur X apibrėžia, kuri raidė yra pirma masyve (t.y. jei X = 1,  tada pirmoji raidė yra A, jei X = 5, tada pirmoji raidė yra Č ir t.t.).
+
+Raidė užkoduojama paimant raidės vietą Y lietuviškoje abėcėlėje (A=1, B=3, D=6 ir t.t.) ir pažiūrint kokia raidė yra vietoje Y pastumtame masyve (t.y., jeigu masyvo X yra 2, o Y yra 1, tai A pavirstų Ą, B pavirstų C ir t.t.).
+
+Raidė taip pat užkoduota per visus 3 masyvus, po kurių pirmasis masyvas perkeliamas viena pozicija, o antrasis masyvas perkeliamas tik tada, kai pirmasis masyvas visiškai apsisuka, o trečiasis – tik tada, kai antrasis visiškai apsisuka.
+
+Pavyzdžiui, jei X1 = 5, X2 = 6, X3 = 9, masyvai atrodys taip:
+
+[Č,D,E,Ę,Ė,F,G,H,I,Į,Y,J,K,L,M,N,O,P,R,S,Š,T,U,Ų,Ū,V,Z,Ž,A,Ą,B,C]
+
+[D,E,Ę,Ė,F,G,H,I,Į,Y,J,K,L,M,N,O,P,R,S,Š,T,U,Ų,Ū, V,Z,Ž,A,Ą,B,C,Č]
+
+[Ė,F,G,H,I,Į,Y,J,K,L,M,N,O,P,R,S,Š,T,U,Ų,Ū,V,Z,Ž,A,Ą,B,C,Č,D,E,Ę]
+
+Antys išsiuntė žinią savo šeimai, tačiau pamiršo, kokios pradinės masyvų pozicijos buvo naudojamos koduojant tekstą. Vienintelis dalykas, kurį jie žino, yra tai, kad pranešimas prasideda TELIA. Žemiau – ančių žinutė.
+
+Kokios buvo pradinės pozicijos, naudojamos tekstui koduoti? Atsakymų langelyje įveskite tris skaičius, atskirdami tarpu.
+
+ĄJŲSIVBOČĄCĮRPĮYPŽEHĖKZIVĘYNZŪAEĮČŽDUUDĘĄMIAĖRKIGEŲRIŠČĖUYKTŲMĮŠR
+
+*/
 
 package main
--- cmd/uzduotis6/main.go
+++ cmd/uzduotis6/main.go
@@ -1,5 +1,197 @@
 package main
 
+var (
+	mp          = map[rune]int{}
+	mp1         = map[rune]int{}
+	mp2         = map[rune]int{}
+	mp3         = map[rune]int{}
+	mas         = "AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ"
+	masr        = []rune(mas)
+	mas1        = "AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ"
+	mas2        = "AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ"
+	mas3        = "AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ"
+	lenght      = 32
+	x           = 1
+	first       = mas[x-1]
+	x1          = 1
+	x2          = 1
+	x3          = 1
+	question    = "ĄJŲSIVBOČĄCĮRPĮYPŽEHĖKZIVĘYNZŪAEĮČŽDUUDĘĄMIAĖRKIGEŲRIŠČĖUYKTŲMĮŠR"
+	questionr   = []rune(question)
+	ques2       = "VBOČĄCĮRPĮYPŽEHĖKZIVĘYNZŪAEĮČŽDUUDĘĄMIAĖRKIGEŲRIŠČĖUYKTŲMĮŠR"
+	ques2r      = []rune(ques2)
+	encryp      = "ĄJŲSI"
+	encrypr     = []rune(encryp)
+	result      = "TELIA"
+	resultr     = []rune(result)
+	result2     = "TELIAKODOKLUBASSVEIKINAĮVEIKUSUŽ"
+	resultr2    = []rune(result2)
+	mqp         = map[rune]int{}
+	resultfull  = "TELIAKODOKLUBASSVEIKINAĮVEIKUSUŽDUOTĮIRSKATINATOLIAUJUDĖTIĮPRIEKĮ"
+	resultfullr = []rune(resultfull)
+	asd         = "TELIAKODOKLUBASSVEIKINAĮVEIKUSUŽČUOTĮIRSKATINATOLIAUJUDĖTIĮPRIEKI"
+	asdr        = []rune(asd)
+)
+
 func main() {
-	println("sesta")
+	for i, a := range masr {
+		mp[a] = i + 1
+	}
+	// sudek(32, 31, 1)
+	// return
+	pradzia(30, 11, 2)
+	return
+
+	// println("sesta")
+	println(string(first))
+	for x33 := 1; x33 <= len(masr); x33++ {
+		for x22 := 1; x22 <= len(masr); x22++ {
+			for x11 := 1; x11 <= len(masr); x11++ {
+				pradzia(x11, x22, x33)
+				// writeme(x11, x22, x33)
+			}
+		}
+	}
+}
+
+// TELIAKODOKLUBASSVEIKINAĮVEIKUSUŽČUOTĮIRSKATINATOLIAUJUDĖTIĮPRIEKI
+
+func pradzia(c1, c2, c3 int) {
+	i1, i2, i3 := c1, c2, c3
+	var runes []rune
+	for i := 0; i < len(resultr2); i++ {
+		sudek(c1, c2, c3)
+		let := resultr2[i]
+		num1 := mp[let]
+		// if c1 == 1 && c2 == 5 && c3 == 1 {
+		// 	println("we")
+		// }
+		for k, v := range mp1 {
+			if v == num1 {
+				let = k
+				break
+			}
+		}
+		// c1 = c1%32 + 1
+		// if c1 == i1 {
+		// 	c2 = c2%32 + 1
+		// 	if c2 == i2 {
+		// 		c3 = c3%32 + 1
+		// 	}
+		// }
+		// sudek(c1, c2, c3)
+		num2 := mp[let]
+		for k, v := range mp2 {
+			if v == num2 {
+				let = k
+				break
+			}
+		}
+		// c1 = c1%32 + 1
+		// if c1 == i1 {
+		// 	c2 = c2%32 + 1
+		// 	if c2 == i2 {
+		// 		c3 = c3%32 + 1
+		// 	}
+		// }
+		// sudek(c1, c2, c3)
+		num3 := mp[let]
+		for k, v := range mp3 {
+			if v == num3 {
+				let = k
+				break
+			}
+		}
+		me := questionr[i]
+		if let != me {
+			return
+		}
+		c1 = c1%32 + 1
+		if c1 == i1 {
+			c2 = c2%32 + 1
+			if c2 == i2 {
+				c3 = c3%32 + 1
+			}
+		}
+		runes = append(runes, let)
+	}
+	// c1 rotated
+	//if string(runes) == question {
+	println(string(runes), writeme(i1, i2, i3), i1, i2, i3)
+	writeme(i1, i2, i3)
+	//}
+	/*if c1 != 1 && c2 != 1 && c3 != 1 {
+		println(c1, c2, c3)
+	}*/
+	//writeme(i1, i2, i3)
+}
+
+// "ĄJŲSIVBOČĄCĮRPĮYPŽEHĖKZIVĘYNZŪAEĮČŽDUUDĘĄMIAĖRKIGEŲRIŠČĖUYKTŲMĮŠR"
+// "TELIA"
+//
+func writeme(c1, c2, c3 int) string {
+	i1, i2 := c1, c2
+	// asd := "TELIAKODOKLUBASSVEIKINAĮVEIKUSUŽDUOTĮIRSKATINATOLIAUJUDĖTIĮPRIEKĮ"
+	// asd = "TELIAKODOKLUBASSVEIKINAĮVEIKUSUŽČUOTĮIRSKATINATOLIAUJUDĖTIĮPRIEKI"
+	// asd = asd
+	var runes []rune
+	for i := 0; i < len(questionr); i++ {
+		sudek(c1, c2, c3)
+		let := questionr[i]
+
+		num3 := mp3[let]
+		for k, v := range mp {
+			if v == num3 {
+				let = k
+				break
+			}
+		}
+		num2 := mp2[let]
+		for k, v := range mp {
+			if v == num2 {
+				let = k
+				break
+			}
+		}
+		num1 := mp1[let]
+		for k, v := range mp {
+			if v == num1 {
+				let = k
+				break
+			}
+		}
+
+		c1 = c1%32 + 1
+		if c1 == i1 {
+			c2 = c2%32 + 1
+			if c2 == i2 {
+				c3 = c3%32 + 1
+			}
+		}
+
+		runes = append(runes, let)
+	}
+
+	return string(runes)
+	// if string(runes) == asd {
+	// 	println(string(runes))
+	// }
+}
+
+func sudek(c1, c2, c3 int) {
+	for i := 0; i < len(masr); i++ {
+		num := (32+i-c1+1)%32 + 1
+		let := masr[i]
+		mp1[let] = num
+	}
+	for i := 0; i < len(masr); i++ {
+		num := (32+i-c2+1)%32 + 1
+		let := masr[i]
+		mp2[let] = num
+	}
+	for i := 0; i < len(masr); i++ {
+		num := (32+i-c3+1)%32 + 1
+		let := masr[i]
+		mp3[let] = num
+	}
 }
